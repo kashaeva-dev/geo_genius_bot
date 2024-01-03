4d4952bb4fe1f550da73332dd5f3e15beb4042dd
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2024-01-03 23:45:21 +0300

    Add registration

--- definitions/management/commands/bot/user_keyboards.py
+++ definitions/management/commands/bot/user_keyboards.py
@@ -1,1 +1,20 @@
 import logging
+import emoji
+
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+user_register_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text='Зарегистрироваться', callback_data='user_register'),
+    ],
+])
+
+user_main_keyboard = InlineKeyboardMarkup(inline_keyboard=[
+    [
+        InlineKeyboardButton(text=emoji.emojize(':eyes: Просмотреть'), callback_data='look_definitions'),
+    ],
+    [
+        InlineKeyboardButton(text=emoji.emojize(':brain: Учить'), callback_data='learn_definitions'),
+    ],
+])
+
